8b1ede713da5dd821376c5f2425e45c973739850
Nhan Nguyen Trong <89365881+ntn145@users.noreply.github.com>
2022-08-11 20:24:16 +0700

    Initial Commit

---
+++ Day7/Day7App.swift
@@ -0,0 +1,17 @@
+//
+//  Day7App.swift
+//  Day7
+//
+//  Created by Khoai Alpha on 11/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Day7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
